62800b19b7b2163bee6fb5b64e605900e4a8ae4f
airsurfish <airsurfish@126.com>
2019-06-13 11:31:23 +0800

    build

---
+++ build/precache-manifest.851f2bdace458fabd8e708ed84955259.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "843b723a7224edab0fc4e88d6f9b6c04",
+    "url": "/firstapp/index.html"
+  },
+  {
+    "revision": "cd5616e5dc071274cb82",
+    "url": "/firstapp/static/css/main.11dde369.chunk.css"
+  },
+  {
+    "revision": "3a9e33913cab0720716d",
+    "url": "/firstapp/static/js/2.86a2265c.chunk.js"
+  },
+  {
+    "revision": "cd5616e5dc071274cb82",
+    "url": "/firstapp/static/js/main.c9ed2e7c.chunk.js"
+  },
+  {
+    "revision": "d2d2977f34597c6756b3",
+    "url": "/firstapp/static/js/runtime~main.bdbdc5eb.js"
+  }
+]);
