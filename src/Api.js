96ab412b764b7b6c7f151fff9f5848f27493adc4
airsurfish <airsurfish@126.com>
2019-06-13 11:25:40 +0800

    test

---
+++ src/Api.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+    state = {
+        data: []
+    };
+
+    componentDidMount() {
+        const url = "https://ghibliapi.herokuapp.com/films";
+
+        fetch(url)
+        .then(result => result.json())
+        .then(result => {
+            this.setState({
+                data: result
+            })
+        });
+    }
+
+    render() {
+        const {data} = this.state;
+
+        const result = data.map((entry, index) => {
+            return <li key={index}>{entry}</li>;
+        });
+
+        return <ul>{result}</ul>;
+    }
+}
+
+export default App;
